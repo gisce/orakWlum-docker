626db8d1f407f21c56bd49130edf802092162b9c
Xavi Torello <info@xaviertorello.cat>
2017-05-29 15:39:42 +0200

    Start script just start an existing container

--- services/api/start.sh
+++ services/api/start.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
+# Load settings
 PATHH=`dirname $0`
 . $PATHH/../settings.sh
 
-# Preventive stop
-bash $PATHH/stop.sh
-
-# Prepare sockets and logs path
-mkdir -p $socket_path
-mkdir -p $API_logs_destination
-
-container_id=`docker run --restart=always --network=host -d $sockets $API_logs --name $API_container -i $API_image`
-
-if [ "$container_id" != "" ]
-then
-    chmod -R 777 $socket_path
-    echo "$container_id" > $API_pid
-    echo "$API_container started! id: "$container_id
+name=`docker start $API_container`
+if [ "$name" == $API_container ]; then
+    echo -e " --->\tStarted container '$name'"
 else
-    if [ -e "id_frontend" ]; then
-        rm $API_pid
-    fi
-    echo "[Error] $API_container can't be started!"
+    echo -e " --->\tCould not start '$API_container'! It exist?"
 fi
-
