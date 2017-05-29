05f11bd31ef50348e946622ced1fdf45b4e8fb34
Xavi Torello <info@xaviertorello.cat>
2017-05-29 17:54:06 +0200

    Use $PATHH at build API

--- services/api/build.sh
+++ services/api/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cp ~/.ssh/id_rsa* ./ssh/
+
+PATHH=`dirname $0`
+
 if [[ $# -gt 0 ]]
 then
-  docker build -t okw/api:$1 --build-arg ORAKWLUM_API_VERSION=$1 .
+  docker build -t okw/api:$1 --build-arg ORAKWLUM_API_VERSION=$1 $PATHH
 else
-  docker build -t okw/api:latest .
+  docker build -t okw/api:latest $PATHH
 fi
 
