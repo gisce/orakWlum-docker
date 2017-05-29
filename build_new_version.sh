c94c90fc5124429054daa266afd80df87537c0ba
Xavi Torello <info@xaviertorello.cat>
2017-05-29 17:52:03 +0200

    Add generic service image builder!

---
+++ build_new_version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ge 1 ]; then
+    service="$1"
+    tag="master"
+   
+    # Validate service
+    if [ "$service" != "api" ] && [ "$service" != "frontend" ] ; then
+        echo "Service must be 'frontend' or 'api'"
+        exit
+    fi
+
+    # Fetch tag / branch to deploy
+    if [ "$#" -ge 2 ]; then
+        tag="$2"
+    fi
+else
+    echo "Service must be provided as 1st argument"
+    exit
+fi
+
+PATHH=`dirname $0`
+echo $PATHH"/services/"$service"/build.sh" $tag 
