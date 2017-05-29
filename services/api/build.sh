f7479b8aeed43da2376e90cba5dc0fddf483db70
Xavi Torello <info@xaviertorello.cat>
2017-05-29 16:26:19 +0200

    Rename default API tag to latest

--- services/api/build.sh
+++ services/api/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp ~/.ssh/id_rsa* ./
+cp ~/.ssh/id_rsa* ./ssh/
 if [[ $# -gt 0 ]]
 then
   docker build -t okw/api:$1 --build-arg ORAKWLUM_API_VERSION=$1 .
 else
-  docker build -t okw/api:master .
+  docker build -t okw/api:latest .
 fi
 
