c4963e8ff60cecc2d42fe41143daf0507a9e22a4
Xavi Torello <info@xaviertorello.cat>
2017-05-29 15:39:25 +0200

    Add API create script

---
+++ services/api/create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PATHH=`dirname $0`
+. $PATHH/../settings.sh
+
+# Preventive stop
+bash $PATHH/stop.sh
+
+# Prepare sockets and logs path
+mkdir -p $socket_path
+mkdir -p $API_logs_destination
+
+container_id=`docker run --restart=always --network=host -d $sockets $API_logs --name $API_container -i $API_image`
+
+if [ "$container_id" != "" ]
+then
+    chmod -R 777 $socket_path
+    echo "$container_id" > $API_pid
+    echo "$API_container started! id: "$container_id
+else
+    if [ -e "id_frontend" ]; then
+        rm $API_pid
+    fi
+    echo "[Error] $API_container can't be started!"
+fi
+
