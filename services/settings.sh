aff13315dbe03b0f79f74165fc3a0a2d686c1630
Xavi Torello <info@xaviertorello.cat>
2017-07-10 20:29:28 +0200

    Add WORKER service settings

--- services/settings.sh
+++ services/settings.sh
@@ -16,3 +16,10 @@ API_container="api"
 API_pid=$socket_path/$API_container.pid
 API_logs_destination="$base_logs/api"
 API_logs="-v $API_logs_destination:/var/log/orakwlum"
+
+# Worker conf
+WORKER_image="okw/worker"
+WORKER_container="worker"
+WORKER_pid=$socket_path/$WORKER_container.pid
+WORKER_logs_destination="$base_logs/worker"
+WORKER_logs="-v $WORKER_logs_destination:/var/log/orakwlum"
